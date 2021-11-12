5b44e1d24c42533d7631eb03d3f2058ddd478de7
DevOps School <44023974+devops-school@users.noreply.github.com>
2021-11-12 13:39:57 +0530

    Add files via upload

---
+++ module/module1/main.tf
@@ -0,0 +1,10 @@
+
+variable "reponame1" {
+  type = string
+  description = "This is for demo of string variable"
+}
+
+resource "github_repository" "example3" {
+  name        = "${var.reponame1}"
+  description = "My awesome codebase"
+  }
